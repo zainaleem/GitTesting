9ce90fa938105e8a0c92e30670f354afb31bbe31
aleemzai <zain.aleem@flui.co.uk>
2023-08-25 18:30:04 +0500

    third commit

--- demo/src/main/java/controller/HelloController.java
+++ demo/src/main/java/controller/HelloController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
-public String getWelcome(){
-
-    return "HelloWorld";
-}
-
+    public String getWelcome() {
+        System.out.println("Yes it is ");
+        System.out.println("No it is not ");
+        System.out.println("learning git ");
+        return "HelloWorld";
+    }
 }
